e8e8c40169b7419871e7f7f90c442e65474253e1
QurratulAin92 <57381048+QurratulAin92@users.noreply.github.com>
2020-06-27 11:38:26 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function getnumber(num){
+    var result = document.getElementById("result");
+    result.value += num;
+}
+function clear_result(){
+    var result = document.getElementById("result");
+    result.value = ""
+}
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
